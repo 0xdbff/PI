95e0677a73f4bbd25cb46f686666fb4f40ffeebf
db <diogo.bernardo.dev@gmail.com>
2022-01-17 10:33:16 +0000

    updated data.c

--- data.c
+++ data.c
@@ -1,13 +1,14 @@
 #include "vec.h"
 
 void read_vehicles(Vehicles *v) {
-  FILE *fp = fopen("./v_ex.txt", "r");
+  FILE *fp = fopen("./data/vehicles.tsv", "r");
+  if (ferror(fp)) {
+    LOG_ERRNO(2);
+    return;
+  }
   char *line = NULL;
   size_t len;
   ssize_t read;
-
-  if (ferror(fp))
-    LOG_ERRNO(2);
 
   char *id = malloc(sizeof(char) * VEHICLE_ID_MAX_CHARS);
   char *type = malloc(sizeof(char) * VEHICLE_TYPE_MAX_CHARS);
@@ -29,14 +30,28 @@ void read_vehicles(Vehicles *v) {
   fclose(fp);
 }
 
+void write_vehicles(Vehicles *v) {
+  FILE *fp = fopen("./data/vehicles.tsv", "w+");
+  if (ferror(fp)) {
+    LOG_ERRNO(2);
+    return;
+  }
+  for (size_t i = 0; i < v->len; i++) {
+    fprintf(fp, "%s\t%s\t%f\t%u\n", (&v->data[i])->id, (&v->data[i])->type,
+            (&v->data[i])->price, (&v->data[i])->autonomy);
+  }
+  fclose(fp);
+}
+
 void read_orders(Orders *v) {
-  FILE *fp = fopen("./data/.txt", "r");
+  FILE *fp = fopen("./data/orders.tsv", "r");
+  if (ferror(fp)) {
+    LOG_ERRNO(2);
+    return;
+  }
   char *line = NULL;
   size_t len;
   ssize_t read;
-
-  if (ferror(fp))
-    LOG_ERRNO(2);
 
   size_t id = 0;
   size_t nif = 0;
@@ -56,5 +71,19 @@ void read_orders(Orders *v) {
   if (line)
     free(line);
   free(v_id);
+  fclose(fp);
+}
+
+void write_orders(Orders *v) {
+  FILE *fp = fopen("./data/orders.tsv", "w+");
+  if (ferror(fp)) {
+    LOG_ERRNO(2);
+    return;
+  }
+  for (size_t i = 0; i < v->len; i++) {
+    fprintf(fp, "%lu\t%lu\t%s\t%u\t%u\n", (&v->data[i])->id, (&v->data[i])->nif,
+            ((&v->data[i])->v_id)->id, (&v->data[i])->time,
+            (&v->data[i])->distance);
+  }
   fclose(fp);
 }
--- tp.c
+++ tp.c
@@ -1,10 +1,12 @@
 #include "vec.h"
 
+//! UTILS
+
 void clean_stdin() {
-  for (int c; c = getchar() != '\n' && c != EOF;)
+  for (int c; (c = getchar()) != '\n' && c != EOF;)
     ;
 }
-
+// inline
 Order *search_order_by_id(Orders *v, const size_t id) {
   for (size_t i = 0; i < v->len; i++) {
     if (id == (&v->data[i])->id)
@@ -12,15 +14,17 @@ Order *search_order_by_id(Orders *v, const size_t id) {
   }
   return NULL;
 }
-
+// inline
 Order *search_order_by_nif(Orders *v, const size_t nif, size_t *pos) {
-  for (size_t i = 0; i < v->len; i++) {
-    if (nif == (&v->data[i])->nif)
+  for (size_t i = *pos; i < v->len; i++) {
+    if ((nif == (&v->data[i])->nif) && (i >= *pos)) {
+      *pos = i + 1;
       return &v->data[i];
+    }
   }
   return NULL;
 }
-
+// inline
 Vehicle *search_vehicle_by_id(Vehicles *v, const char *id) {
   for (size_t i = 0; i < v->len; i++) {
     if ((strcmp(id, (&v->data[i])->id)) == 0)
@@ -28,7 +32,7 @@ Vehicle *search_vehicle_by_id(Vehicles *v, const char *id) {
   }
   return NULL;
 }
-
+// inline
 Vehicle *search_vehicle_by_type(Vehicles *v, const char *type, size_t *pos) {
   for (size_t i = *pos; i < v->len; i++) {
     if (((strcmp(type, (&v->data[i])->type)) == 0) && (i >= *pos)) {
@@ -39,3 +43,4 @@ Vehicle *search_vehicle_by_type(Vehicles *v, const char *type, size_t *pos) {
   return NULL;
 }
 
+//! 01
